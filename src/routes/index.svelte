b34fefd69100b591b3d3be7ee3a3e508150dcab4
jaydontelebort <jaydon.telebort@gmail.com>
2021-10-28 09:26:31 +0000

    first commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	// import supabase from '$lib/db';
+    // import {session} from '$app/stores';
+
+	// let email, password;
+	// async function signUp() {
+	// 	const { user, session: sesh, error } = await supabase.auth.signUp({
+	// 		email,
+	// 		password
+	// 	});
+	// 	if (error) alert(error.message);
+	// 	console.log(user, session, error);
+	// 	$session = sesh;
+	// }
+</script>
+
+<!-- <form on:submit|preventDefault={signUp}>
+	<label for="email">Email</label>
+	<input id="email" bind:value={email} type="email" />
+
+	<label for="password">Password</label>
+	<input id="password" bind:value={password} type="password" />
+
+	<button>Sign Up</button>
+</form> -->
